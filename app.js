157a34c88ef5e44c6c348dcaaf4f0cdc16f98536
Op Pautu <pautuop@gmail.com>
2023-02-27 23:33:41 +0530

    Set view engine

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Listening on port 3000');
+});
